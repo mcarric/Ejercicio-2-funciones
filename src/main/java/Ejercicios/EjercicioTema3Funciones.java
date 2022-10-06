c1c359e7a2dc3b8425cb2d19c2e5577999626d03
mcarric <maitecarricaburu@gmail.com>
2022-10-06 07:17:56 -0300

    Ejercicio Funciones tema 3

---
+++ src/main/java/Ejercicios/EjercicioTema3Funciones.java
@@ -0,0 +1,17 @@
+package Ejercicios;
+
+public class EjercicioTema3Funciones {
+    public static void main(String[] args) {
+
+        String[] names = {"Maite", "Lisandro", "Angelina", "Pepe Grillo"};
+
+        for (int i = 0; i < names.length; i++) {
+
+            System.out.println(names[i]);
+
+        }
+
+    }
+}
+
+
